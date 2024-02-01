78ea3b156aac5269a671f720a8f31a647e92d01a
AhmidouA <ahmedakkioui75@gmail.com>
2024-02-01 08:50:08 +0100

    logout Done

---
+++ include/logout.php
@@ -0,0 +1,17 @@
+<?php 
+session_start();
+
+if(isset($_SESSION['id'])) {
+    // Detruire les variable de la sessions
+    session_unset();
+    // Detruire la session elle meme
+    session_destroy();
+
+    echo '<script type="text/javascript"> alert("You are disconnected"); </script>';
+    header("Refresh: 3; URL=/index.php");
+} else {
+    echo '<script type="text/javascript"> alert("You are not connected"); </script>';
+    header("Refresh: 2; URL=/index.php");
+}
+
+?>
--- index.php
+++ index.php
@@ -1,16 +1,27 @@
 <?php require 'include/header.html'; ?>
 <title>Home Page</title>
+
 <body>
-    <div class="session">
-        <?php
-        session_start();
-        if(isset($_SESSION['id'])) {
-            echo 'Hello ' .$_SESSION['username'];
-        }
-        ?>
-    </div>
     <div class="home">
-        <h1 class="title-home"><a href="include/signup.php">Signup</a> <br></h1>
-        <h1 class="title-home"> <a href="include/login.php">Login</a> <br></h1>
+        <div class="session">
+            <?php
+            session_start();
+            if (isset($_SESSION['id'])) {
+                echo 'Hello ' . $_SESSION['username'];
+            ?>
+        </div>
+        <h1 class="title-home"><a href="include/logout.php">Logout</a> </h1><br>
+
+    <?php
+            } else {
+    ?> 
+
+        <div class="disconnected">
+            <h1 class="title-home"><a href="include/signup.php">Signup</a></h1> <br>
+            <h1 class="title-home"><a href="include/login.php">Login</a></h1> <br>
+        </div>
+    <?php
+            }
+    ?>
     </div>
 </body>
